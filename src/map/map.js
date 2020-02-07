8179c9e34d168252447d367184a07d630c00b9f3
Jerry Osorio <51134653+JerryOsorio28@users.noreply.github.com>
2020-02-07 15:08:31 -0500

    initial commit

---
+++ src/map/map.js
@@ -0,0 +1,18 @@
+import React, { useCallback, useState } from 'react'
+import Rooms from '../rooms/rooms'
+import './map.scss'
+
+const GameMap = props => {
+
+    // console.log('MAP ROOMS', props.rooms)
+
+    return (
+        <div className='gameMap'>
+            <div className='grid'>
+                <Rooms rooms={props.rooms} setRooms={props.setRooms}/>
+            </div>
+        </div>
+    )
+}
+
+export default GameMap
